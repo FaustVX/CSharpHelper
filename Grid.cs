b44d2db9b3ee2b500a8ed220e72e4378b0a9a8d1
Jonathan <jodu035@gmail.com>
2013-08-31 06:07:55 +0200

    Ajout de commentaires pour Grid.cs

--- Grid.cs
+++ Grid.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
-
-namespace CSharpHelper
+﻿namespace CSharpHelper
 {
 
-	public abstract class Cell<T> : Grid<T>.ICell
-		where T : class, Grid<T>.ICell
+	/// <summary>
+	/// classe de cellules basique 
+	/// </summary>
+	/// <typeparam name="T">Type de cellules, héritant de la classe <see cref="Cell{T}"/></typeparam>
+	public class Cell<T> : Grid<T>.ICell
+		where T : Cell<T>
 	{
 		private readonly int _x, _y;
 		private T _up, _left, _down, _right;
 
-		protected Cell(int x, int y, Grid<T> list, bool taurus)
+		/// <summary>
+		/// Constructeur de la  cellule
+		/// </summary>
+		/// <param name="x">position en X</param>
+		/// <param name="y">position en Y</param>
+		/// <param name="list">liste de cellules <typeparamref name="T"/></param>
+		/// <param name="torus">indique si les cellules agissent comme un torus(les cellules du haut sont liées aux cellules du bas, et vics-versa, pareil pour la gauche et la droite)</param>
+		protected Cell(int x, int y, Grid<T> list, bool torus)
 		{
 			_x = x;
 			_y = y;
@@ -18,7 +27,7 @@ namespace CSharpHelper
 			{
 				_left = list[x - 1, y];
 				(Left as Cell<T>)._right = this as T;
-				if (taurus && X == list.Width - 1)
+				if (torus && X == list.Width - 1)
 				{
 					_right = list[0, y];
 					(Right as Cell<T>)._left = this as T;
@@ -29,7 +38,7 @@ namespace CSharpHelper
 			{
 				_up = list[x, y - 1];
 				(Up as Cell<T>)._down = this as T;
-				if (taurus && Y == list.Height - 1)
+				if (torus && Y == list.Height - 1)
 				{
 					_down = list[x, 0];
 					(Down as Cell<T>)._up = this as T;
@@ -68,13 +77,24 @@ namespace CSharpHelper
 		}
 	}
 
-	public abstract class DiagonalCell<T> : Cell<T>
+	/// <summary>
+	/// classe de cellules basique gérant les diagonales
+	/// </summary>
+	/// <typeparam name="T">Type de cellules, héritant de la classe <see cref="DiagonalCell{T}"/></typeparam>
+	public class DiagonalCell<T> : Cell<T>
 		where T : DiagonalCell<T>
 	{
 		//private T _upLeft, _downLeft, _upRight, _downRight;
 
-		protected DiagonalCell(int x, int y, Grid<T> list, bool taurus)
-			: base(x, y, list, taurus)
+		/// <summary>
+		/// Constructeur de la  cellule
+		/// </summary>
+		/// <param name="x">position en X</param>
+		/// <param name="y">position en Y</param>
+		/// <param name="list">liste de cellules <typeparamref name="T"/></param>
+		/// <param name="torus">indique si les cellules agissent comme un torus(les cellules du haut sont liées aux cellules du bas, et vics-versa, pareil pour la gauche et la droite)</param>
+		protected DiagonalCell(int x, int y, Grid<T> list, bool torus)
+			: base(x, y, list, torus)
 		{ }
 
 		public T UpLeft
@@ -87,13 +107,13 @@ namespace CSharpHelper
 			}
 		}
 
-		public T DownRight
+		public T UpRight
 		{
 			get
 			{
-				if (Down != null)
-					return Down.Right;
-				return Right != null ? Right.Down : null;
+				if (Up != null)
+					return Up.Right;
+				return Right != null ? Right.Up : null;
 			}
 		}
 
@@ -107,22 +127,29 @@ namespace CSharpHelper
 			}
 		}
 
-		public T UpRight
+		public T DownRight
 		{
 			get
 			{
-				if (Up != null)
-					return Up.Right;
-				return Right != null ? Right.Up : null;
+				if (Down != null)
+					return Down.Right;
+				return Right != null ? Right.Down : null;
 			}
 		}
 	}
 
+	/// <summary>
+	/// Classe permettant la gestion de cellules de type <typeparamref name="T"/> dans une grille
+	/// </summary>
+	/// <typeparam name="T">Type de cellules, implémentant l'interface <see cref="ICell"/></typeparam>
 	public class Grid<T>
 		where T : class, Grid<T>.ICell
 	{
 		#region Inner Class Cell
 
+		/// <summary>
+		/// Interface de base pour les cellules
+		/// </summary>
 		public interface ICell
 		{
 			T Up
@@ -162,6 +189,12 @@ namespace CSharpHelper
 		private readonly int _width;
 		private readonly int _height;
 
+		/// <summary>
+		/// Constructeur de la grille
+		/// </summary>
+		/// <param name="width">Nombre de cellules horizontalement</param>
+		/// <param name="height">Nombre de cellules verticalement</param>
+		/// <param name="createCell">constructeur de cellules <typeparamref name="T"/> en fonction de sa position x, y et de la <see cref="Grid"/> de <typeparamref name="T"/></param>
 		protected Grid(int width, int height, System.Func<int, int, Grid<T>, T> createCell)
 		{
 			_width = width;
